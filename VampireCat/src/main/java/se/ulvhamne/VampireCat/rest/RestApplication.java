a0816f2f80c8637da706dd76473abee0bd432a47
Jonas Svensson <wulfhaven@gmail.com>
2019-12-25 15:50:23 +0100

    Compiling, running and responding server. No stuff in there yet.

---
+++ VampireCat/src/main/java/se/ulvhamne/VampireCat/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package se.ulvhamne.VampireCat.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
