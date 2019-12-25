9f5e9dda813653b1a982bf54553b3917ae87aa58
Jonas Svensson <wulfhaven@gmail.com>
2019-12-25 20:14:59 +0100

    added android project

---
+++ VampireCat/src/main/java/se/ulvhamne/VampireCat/data/CatUser.java
@@ -0,0 +1,8 @@
+package se.ulvhamne.VampireCat.data;
+
+public class CatUser {
+    private String username = "Placeholder";
+    private String email = "em@il";
+    private String mobile = "555 000 000";
+    private String Location = "Hell";
+}
